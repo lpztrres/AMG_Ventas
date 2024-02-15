c0eb7c33e067ce6959adb77909567df9347cbdf1
Luis_LPZ <Usuario@DESKTOP-IUSQE53>
2024-02-14 22:54:54 -0600

    Agregar DataContext

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -1,11 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DockerConnection"));
 
 var app = builder.Build();
 
