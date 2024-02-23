1a0dbef10bfd4ee3888981bd4220caccea361e58
Luis_LPZ <Usuario@DESKTOP-IUSQE53>
2024-02-23 14:02:38 -0600

    Versión03.Controladores de la API para habilitar su consumo.

--- Sales.API/Controllers/CountriesController.cs
+++ Sales.API/Controllers/CountriesController.cs
@@ -7,19 +7,19 @@ namespace Sales.API.Controllers
 {
     [ApiController]
     [Route("api/countries")]
-    public class CountriesController : ControllerBase
+    public class CountriesController: ControllerBase
     {
         private readonly DataContext _context;
-        public CountriesController(DataContext context) 
+        public CountriesController(DataContext context)
         {
             _context = context;
         }
         [HttpPost]
-        public async Task<ActionResult> PostAsync(Country country) 
-        {
+        public async Task<ActionResult> Post(Country country) {
             _context.Countries.Add(country);
             await _context.SaveChangesAsync();
             return Ok(country);
+        
         }
         [HttpGet]
         public async Task<IActionResult> GetAsync()
